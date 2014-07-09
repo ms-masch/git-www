04fa6ca6a105cbd6a9ef53a054782ef55c9ba06a
ms-masch <github@xyz.de>
2014-07-09 13:34:12 +0200

    index.php add header

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Beispiel HTML5 Forms</title>
+    <meta charset="UTF-8" />
+    <meta name="description" content="Kurzbeschreibung" />
+    <link href="style.css" type="text/css" rel="stylesheet" />
+</head>
+    
+<body>
 <?php
     echo"TEST";
     
     
     phpinfo();
 ?>
+</body>
+</html>
