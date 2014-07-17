73ddf7460790f3177e949f590e7011396fba7050
ms-masch <github@xyz.de>
2014-07-17 13:21:49 +0200

    f:js.js

--- html5/js.js
+++ html5/js.js
@@ -1,15 +1,14 @@
-function my_array() {
+/*function my_array() {
     //code
     mein_array = new Array();
 }
 
-
+*/
 function my_delete() {
     var a = 10;
         b = 20;
-        delete a;
-        delete b;
+        //delete a;
+       // delete b;
         alert(a);
         alert(b);
 }
-my_delete();
