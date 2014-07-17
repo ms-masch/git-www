4d62df9df101238922abd97716f42285f5be6229
ms-masch <github@xyz.de>
2014-07-17 13:33:37 +0200

    f:js.js

--- html5/js.js
+++ html5/js.js
@@ -1,14 +1,24 @@
-/*function my_array() {
+function my_array() {
     //code
     mein_array = new Array();
 }
 
-*/
 function my_delete() {
+    // var global object
     var a = 10;
         b = 20;
         delete a;
         delete b;
         alert(a);
         alert(b);
+}
+
+function my_typ() {
+    a = 1;
+    b = '2';
+    alert (a + b);
+    
+    a = 1;
+    b = '2';
+    alert (a + Number(b));
 }
